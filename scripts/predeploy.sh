25ad329d093f149ede236271ea1d8338dabd3444
Jose Carlos <solrac.carlos767@gmail.com>
2025-06-22 10:46:29 -0400

    se anadio cambios al script.sh

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -9,25 +9,19 @@ php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
-# Volver a cachear configuraciones, rutas y vistas
+# Compilar assets con Vite
+if [ -f package.json ]; then
+    npm install
+    npm run build
+fi
+
+# Generar la APP_KEY
+php artisan key:generate
+
+# Cachear configuraciones, rutas y vistas
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Instalar dependencias JS y compilar assets si usas npm/yarn
-if [ -f package.json ]; then
-    npm install
-    npm run production
-fi
-
-php artisan key:generate
-
-
-# Ejecutar migraciones forzadas sin interacción (en producción)
+# Ejecutar migraciones forzadas sin interacción
 php artisan migrate --force
-
-# (Opcional) Ejecutar seeders si quieres precargar datos
-# php artisan db:seed --force
-
-# (Opcional) Dar permisos a storage y bootstrap/cache si es necesario
-# chmod -R 775 storage bootstrap/cache
