a7fa54a7844738952ef6b8cb48cfc5a917721abb
Jose Carlos <solrac.carlos767@gmail.com>
2025-08-16 09:37:28 -0400

    correccion de vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    base: '/nLecturaMovil/', // Agrega el prefijo aquí
+    base: '/nLecturaMovil/build/', // Agrega el prefijo aquí
     server: {
         watch: {
             usePolling: true,
