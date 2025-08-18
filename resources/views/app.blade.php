7c62c33c63e692c95de31dba7041e0caa079f0a7
Jose Carlos <solrac.carlos767@gmail.com>
2025-08-18 06:58:14 -0400

    nuevos cambios

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,28 +10,18 @@
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+    <!-- CSS locales -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/AdminLTE.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/_all-skins.min.css') }}">
 
-    <!-- Bootstrap 4.0.0 -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css') }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/fontawesome.css') }}">
-    <link rel="stylesheet" href="{{ url('https://use.fontawesome.com/releases/v6.2.0/css/all.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/AdminLTE.min.css') }}">
-    <!--Style datatable -->
-    <link rel="stylesheet" type="text/css"
-        href="{{ asset('https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css') }}" />
+    <!-- CSS externas (no usar asset() para URLs externas) -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-    folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/_all-skins.min.css') }}">
-
-    <!-- Fonts -->
-    <!-- <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
-
-    <!-- Scripts -->
+    <!-- Scripts Vite -->
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
