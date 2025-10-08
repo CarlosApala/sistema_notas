47a9872574d86eb4e6f10739f4bee3d7b1e86aab
carlos@debian <solrac.carlos767@gmail.com>
2025-10-08 08:16:20 -0400

    modifico el controlador

--- app/Http/Controllers/Api/AvisoController.php
+++ app/Http/Controllers/Api/AvisoController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Aviso; // ← Esto es obligatorio
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\Rule;
 
